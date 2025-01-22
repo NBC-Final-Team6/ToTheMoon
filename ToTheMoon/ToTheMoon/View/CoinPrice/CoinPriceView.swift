39a0c89406ed94e9910f290ed6c2e7f504a4454f
Jimin Seo <tig05015@naver.com>
2025-01-22 11:00:55 +0900

    [Feat]: CoinPriceView 클래스 생성

--- ToTheMoon/ToTheMoon/View/CoinPrice/CoinPriceView.swift
+++ ToTheMoon/ToTheMoon/View/CoinPrice/CoinPriceView.swift
@@ -5,3 +5,15 @@
 //  Created by 황석범 on 1/21/25.
 //
 
+import UIKit
+import SnapKit
+
+class CoinPriceView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
