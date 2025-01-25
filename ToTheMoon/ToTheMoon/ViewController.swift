fb9ae08dd83dceb307184243522e3f86e8f6e3cb
HwangSeokBeom <tjrqja0714@gmail.com>
2025-01-25 12:51:08 +0900

    [Fix]: 불필요한 코드 삭제

--- ToTheMoon/ToTheMoon/ViewController.swift
+++ ToTheMoon/ToTheMoon/ViewController.swift
@@ -6,12 +6,8 @@
 //
 
 import UIKit
-import Foundation
-import RxSwift
 
 class ViewController: UIViewController {
-
-    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
