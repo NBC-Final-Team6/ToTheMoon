5e9c12f13a4b4daea3b6afbd69f30876ec5e2a5d
HwangSeokBeom <tjrqja0714@gmail.com>
2025-01-30 20:23:53 +0900

    [Fix]: 불필요 디렉토리 삭제

--- ToTheMoon/ToTheMoon/SceneDelegate.swift
+++ ToTheMoon/ToTheMoon/SceneDelegate.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
     var window: UIWindow?
+
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: CustomTabBarViewController())
+        window.rootViewController = UINavigationController(rootViewController: SearchViewController(viewModel: <#SearchViewModel#>))
         window.makeKeyAndVisible()
         self.window = window
     }
