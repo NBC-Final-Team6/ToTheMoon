f6b30fd09c97c7433acb6a169609e4ad5583b8b4
Jimin Seo <tig05015@naver.com>
2025-02-05 16:05:42 +0900

    [Fix]: 마켓 레이블 높이 수정, print문 제거

--- ToTheMoon/ToTheMoon/View/CoinPrice/CoinPriceViewController.swift
+++ ToTheMoon/ToTheMoon/View/CoinPrice/CoinPriceViewController.swift
@@ -134,7 +134,6 @@ class CoinPriceViewController: UIViewController {
         viewModel.error
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { error in
-                print("Error occurred: \(error.localizedDescription)")
             })
             .disposed(by: disposeBag)
     }
