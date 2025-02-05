4e29c019378a80f01df91711571cd189dfad71e1
kangminseoung <alstjd2356@gmail.com>
2025-02-05 10:16:37 +0900

    [Feat] : 네비게이션 바 기능 구현

--- ToTheMoon/ToTheMoon/View/Chart/ChartViewController.swift
+++ ToTheMoon/ToTheMoon/View/Chart/ChartViewController.swift
@@ -38,12 +38,32 @@ class ChartViewController: UIViewController {
         setupViews()
         setupBindings()
         bindViewModel()
+        setupNavigationBar()
+        navigationController?.navigationBar.isHidden = false
         
         if let firstCoin = try? viewModel.selectedCoins.value().first {
             viewModel.fetchCandles(for: firstCoin)
         }
         
         updateSelectedTimeFrame(.day)
+    }
+
+    // 네비게이션 바 및 뒤로가기 버튼 설정
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.navigationBar.tintColor = .black // 뒤로가기 버튼 색상
+
+        let backButton = UIBarButtonItem(
+            image: UIImage(systemName: "chevron.left"), // 뒤로가기 아이콘
+            style: .plain,
+            target: self,
+            action: #selector(backButtonTapped)
+        )
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
     }
 
     private func setupViews() {
