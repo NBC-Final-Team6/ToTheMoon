7ef2e0c37b736c95835ea88d4c61c1f9f2e80cca
HwangSeokBeom <tjrqja0714@gmail.com>
2025-02-12 10:01:35 +0900

    [Feat]: BaseService 수정, Service 프로토콜 구현

--- ToTheMoon/ToTheMoon/Services/Common/BaseService.swift
+++ ToTheMoon/ToTheMoon/Services/Common/BaseService.swift
@@ -16,10 +16,14 @@ class BaseService {
         self.baseURL = baseURL
     }
 
-    func request<T: Decodable>(endpoint: String) -> Single<T> {
-        guard let url = URL(string: baseURL + endpoint) else {
+    func request<T: Decodable>(endpoint: String, queryParams: [String: String]? = nil) -> Single<T> {
+        var urlComponents = URLComponents(string: baseURL + endpoint)
+        urlComponents?.queryItems = queryParams?.map { URLQueryItem(name: $0.key, value: $0.value) }
+
+        guard let url = urlComponents?.url else {
             return Single.error(NetworkError.invalidUrl)
         }
+
         return networkManager.fetch(url: url)
     }
 }
---
+++ ToTheMoon/ToTheMoon/Services/Common/ServiceProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  Untitled.swift
+//  ToTheMoon
+//
+//  Created by 황석범 on 2/12/25.
+//
+
+import RxSwift
+
+protocol ServiceProtocol {
+    var exchange: Exchange { get }
+    
+    func fetchMarketPrices() -> Single<[MarketPrice]>
+    func fetchMarketPrice(symbol: String) -> Single<MarketPrice>
+    func fetchCandles(symbol: String, interval: CandleInterval, count: Int) -> Single<[Candle]>
+}
