4f953996aff59da9419158cd30350a4ab79ddbd1
HwangSeokBeom <tjrqja0714@gmail.com>
2025-02-12 13:34:58 +0900

    [Refactoring]: GetMarketPrciesUseCase도 추상화한 서비스프로토콜에 맞게 수정

--- ToTheMoon/ToTheMoon/UseCase/GetMarketPricesUseCase.swift
+++ ToTheMoon/ToTheMoon/UseCase/GetMarketPricesUseCase.swift
@@ -10,67 +10,50 @@ import RxSwift
 import UIKit
 
 final class GetMarketPricesUseCase {
-    private let bithumbService: BithumbService
-    private let coinOneService: CoinOneService
-    private let korbitService: KorbitService
-    private let upbitService: UpbitService
+    private let services: [ServiceProtocol]
     private let symbolService: SymbolService
     
     init(
-        bithumbService: BithumbService = BithumbService(),
-        coinOneService: CoinOneService = CoinOneService(),
-        korbitService: KorbitService = KorbitService(),
-        upbitService: UpbitService = UpbitService(),
+        services: [ServiceProtocol] = [BithumbService(), CoinOneService(), KorbitService(), UpbitService()],
         symbolService: SymbolService = SymbolService()
     ) {
-        self.bithumbService = bithumbService
-        self.coinOneService = coinOneService
-        self.korbitService = korbitService
-        self.upbitService = upbitService
+        self.services = services
         self.symbolService = symbolService
     }
     
     func execute() -> Single<[MarketPrice]> {
-        let bithumbPrices = bithumbService.fetchMarketPrices()
-        let coinOnePrices = coinOneService.fetchMarketPrices()
-        let korbitPrices = korbitService.fetchMarketPrices()
-        let upbitPrices = upbitService.fetchMarketPrices()
+        let priceObservables = services.map { $0.fetchMarketPrices() }
         
         let symbolFormatter = SymbolFormatter()
         
-        return Single.zip(bithumbPrices, coinOnePrices, korbitPrices, upbitPrices)
-            .flatMap { [weak self] bithumb, coinOne, korbit, upbit -> Single<[MarketPrice]> in
+        return Single.zip(priceObservables)
+            .flatMap { [weak self] marketPricesArray -> Single<[MarketPrice]> in
                 guard let self = self else { return .just([]) }
                 
-                let allPrices = bithumb + coinOne + korbit + upbit
+                let allPrices = marketPricesArray.flatMap { $0 }
                 
-                // ✅ 중복 요청 방지를 위한 Dictionary
                 var imageRequests: [String: Single<UIImage?>] = [:]
                 
                 let updatedMarketPrices = allPrices.map { marketPrice -> Single<MarketPrice> in
-                    // ✅ 심볼 정규화 적용
-                    let normalizedSymbol = symbolFormatter.format(symbol: marketPrice.symbol)
+                    let normalizedSymbol = symbolFormatter.format(symbol: marketPrice.symbol).uppercased()
                     
-                    // ✅ 정규화된 심볼을 기반으로 새로운 MarketPrice 객체 생성
                     var updatedMarketPrice = MarketPrice(
-                        symbol: normalizedSymbol.uppercased(),
+                        symbol: normalizedSymbol,
                         price: marketPrice.price,
                         exchange: marketPrice.exchange,
                         change: marketPrice.change,
                         changeRate: marketPrice.changeRate,
-                        quoteVolume: marketPrice.quoteVolume ,
+                        quoteVolume: marketPrice.quoteVolume,
                         highPrice: marketPrice.highPrice,
                         lowPrice: marketPrice.lowPrice,
-                        image: nil // 초기 이미지 없음
+                        image: nil
                     )
                     
-                    // 1. 이미지 캐시 확인
-                    if let defaultImage = ImageRepository.getImage(for: normalizedSymbol) {
-                        updatedMarketPrice.image = defaultImage
+                    if let cachedImage = ImageRepository.getImage(for: normalizedSymbol) {
+                        updatedMarketPrice.image = cachedImage
                         return Single.just(updatedMarketPrice)
                     }
                     
-                    // 2. 같은 심볼에 대한 요청이 이미 생성되었는지 확인
                     if let existingRequest = imageRequests[normalizedSymbol] {
                         return existingRequest.map { image in
                             updatedMarketPrice.image = image
@@ -78,7 +61,6 @@ final class GetMarketPricesUseCase {
                         }
                     }
                     
-                    // 3. 새로운 네트워크 요청 생성
                     let imageRequest = self.symbolService.fetchCoinThumbImage(coinSymbol: normalizedSymbol)
                         .do(onSuccess: { image in
                             if let image = image {
