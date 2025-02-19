a97b3f6e72dd0be2eb740182fd9a2d18412e0869
“hyunwook-sevo” <seojy1276@naver.com>
2025-02-19 15:19:33 +0900

    update :  뷰 위치도 수정했음

--- ToTheMoon/ToTheMoon/View/TabBar/CustomTabBarViewController.swift
+++ ToTheMoon/ToTheMoon/View/TabBar/CustomTabBarViewController.swift
@@ -12,8 +12,8 @@ import RxCocoa
 
 class CustomTabBarViewController: UIViewController {
     private let customTabBar = CustomTabBarView()
-    private let favoritesNavVC = UINavigationController(rootViewController: FavoritesContainerViewController())
     private let coinPricesNavVC = UINavigationController(rootViewController: CoinPriceViewController())
+    private let favoritesNavVC = UINavigationController(rootViewController: FavoritesContainerViewController())
     private let settingsNavVC = UINavigationController(rootViewController: SettingViewController())
 
     private var currentViewController: UIViewController?
@@ -67,9 +67,9 @@ class CustomTabBarViewController: UIViewController {
 
         switch index {
         case 0:
-            selectedViewController = favoritesNavVC
-        case 1:
             selectedViewController = coinPricesNavVC
+        case 1:
+            selectedViewController = favoritesNavVC
         case 2:
             selectedViewController = settingsNavVC
         default:
